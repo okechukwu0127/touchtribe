be8dc0369016ce444ba1cc9ce65a97c82a3073c9
okechukwu eze <okechukwu0127@gmail.com>
2022-12-13 09:57:50 +0100

    fresh

--- src/components/player/HLSSource.js
+++ src/components/player/HLSSource.js
@@ -1,45 +1,35 @@
 import { useEffect, useState } from "react";
 
 const HLSSource = ({ src, video }, type = "application/x-mpegURL") => {
-  try {
-    const constructorCheck = Object.prototype.hasOwnProperty.call(
-      window,
-      "Hls"
-    );
+  // try {
+  const constructorCheck = Object.prototype.hasOwnProperty.call(window, "Hls");
 
-    if (constructorCheck) {
+  const [prevProps, setPrevProps] = useState();
+
+  useEffect(() => {
+    if (constructorCheck && src !== prevProps) {
       const { Hls } = window;
       const hls = new Hls();
 
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      useEffect(() => {
-        if (src !== prevProps) {
-          setPrevProps(src);
-          if (Hls.isSupported()) {
-            hls.loadSource(src);
-            hls.attachMedia(video);
-            hls.on(Hls.Events.MANIFEST_PARSED, () => {});
-          }
+      //   if (src !== prevProps) {
+      setPrevProps(src);
+      if (Hls.isSupported()) {
+        hls.loadSource(src);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {});
+      }
+     
+
+      return () => {
+        if (hls) {
+          hls.destroy();;
         }
-
-        return () => {
-          if (hls) {
-            hls.destroy();
-          }
-        };
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [src]);
+      };
     }
+  }, [constructorCheck, prevProps, src, video]);
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [prevProps, setPrevProps] = useState();
-
-
-    return <source src={src} type={type} />;
-  } catch (error) {
-    //  console.log(error);
-  }
+  return <source src={src} type={type} />;;
+  
 };
 
 export default HLSSource;
